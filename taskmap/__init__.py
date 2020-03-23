4714d391cdaabc94be7cac86c33b05dd24da21d4
ctlaltdefeat <jgradstein@gmail.com>
2020-03-23 03:25:16 +0200

    Update __init__.py

--- taskmap/__init__.py
+++ taskmap/__init__.py
@@ -1,5 +1,5 @@
 from .taskmap import (run_task, run_task_async, run, run_parallel, run_async,
-                      run_parallel_async)
+                      run_parallel_async, async_run_async)
 
 from .tgraph import (create_graph, get_ready_tasks, mark_as_done,
                      mark_as_done_except, get_all_children, reset_tasks,
